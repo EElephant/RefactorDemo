2cb8a3abe6946032ec5c209ba44305f9f1217f7b
Ethan <ethan.huang@oocl.com>
2019-08-13 17:16:35 +0800

    Refactor: remove the 'switch case' into Item Class

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 public class GildedRose {
 
-    private final static String AGED_BRIE = "Aged Brie";
-    private final static String BPTATC = "Backstage passes to a TAFKAL80ETC concert";
-    private final static String SHOR = "Sulfuras, Hand of Ragnaros";
+
 
     protected List<Item> items;
     protected String name;
@@ -20,48 +18,25 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.size(); i++) {
-
             initParams(i);
-            ItemStrategy itemStrategy;
-
-            switch(name){
-                case AGED_BRIE:
-                    itemStrategy = new AgedBrie();
-                    update(i, itemStrategy);
-                    break;
-
-                case BPTATC:
-                    itemStrategy = new Backstage();
-                    update(i, itemStrategy);
-                    break;
-
-                case SHOR:
-                    itemStrategy = new Sulfuras();
-                    update(i, itemStrategy);
-                    break;
-
-                default:
-                    itemStrategy = new NormalItem();
-                    update(i, itemStrategy);
-                    break;
-            }
+            update(i);
         }
     }
 
-    private void update(int i, ItemStrategy itemStrategy) {
-        itemStrategy.update(items.get(i));
+    private void update(int i) {
+        items.get(i).update(items.get(i));
         deassignParams(items.get(i));
     }
 
-    private void deassignParams(Item item) {
-        quality = item.quality;
-        sellIn = item.sellIn;
-        name = item.name;
-    }
 
     private void initParams(int i) {
         this.name = items.get(i).name;
         this.quality = items.get(i).quality;
         this.sellIn = items.get(i).sellIn;
+    }
+    private void deassignParams(Item item) {
+        quality = item.quality;
+        sellIn = item.sellIn;
+        name = item.name;
     }
 }
--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -2,6 +2,10 @@ package gildedRose;
 
 public class Item {
 
+    private final static String AGED_BRIE = "Aged Brie";
+    private final static String BPTATC = "Backstage passes to a TAFKAL80ETC concert";
+    private final static String SHOR = "Sulfuras, Hand of Ragnaros";
+
     public String name;
 
     public int sellIn;
@@ -14,8 +18,40 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
-        return this.name + ", " + this.sellIn + ", " + this.quality;
+    public void update(Item item){
+        ItemStrategy itemStrategy;
+
+        switch(name){
+            case AGED_BRIE:
+                itemStrategy = new AgedBrie();
+                updateParmas(item, itemStrategy);
+                break;
+
+            case BPTATC:
+                itemStrategy = new Backstage();
+                updateParmas(item, itemStrategy);
+                break;
+
+            case SHOR:
+                itemStrategy = new Sulfuras();
+                updateParmas(item, itemStrategy);
+                break;
+
+            default:
+                itemStrategy = new NormalItem();
+                updateParmas(item, itemStrategy);
+                break;
+        }
     }
+
+    private void updateParmas(Item item, ItemStrategy itemStrategy) {
+        itemStrategy.update(item);
+    }
+
+    private void deassignParams(Item item) {
+        quality = item.quality;
+        sellIn = item.sellIn;
+        name = item.name;
+    }
+
 }
